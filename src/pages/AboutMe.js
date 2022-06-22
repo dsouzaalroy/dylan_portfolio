5aebcbc935de9228ec2b8641cb2743b9229a2c6a
dsouza_alroy <alroy133@gmail.com>
2022-06-22 17:03:16 +0100

    Changed pdf

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -17,7 +17,7 @@ class AboutMe extends Component {
                     <div className = 'description'>
                         <p className = 'hello'>Hello</p>
                         <p className= 'about_text'>My name is <b>Dylan Fernandes</b>, I'm a <b>Digital Designer</b> based in India</p>
-                        <p className= 'about_text'>I'm currently studying at Rachana Sansad College of Applied Art and craft</p>
+                        <p className= 'about_text'>I'm currently not studying at Rachana Sansad College of Applied Art and craft</p>
                     </div>
                     <div className='CV'>
                     <a href={dylan_cv} target="_blank"><p className="hover">View my CV</p></a>
